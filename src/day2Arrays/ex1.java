4f9e5e38425bbe2c5b640b9defc35db59f33ee6f
jayesh sarvaiya <jayeshsarvaiya9913@gmail.com>
2024-02-13 11:06:03 +0530

    first commit

---
+++ src/day2Arrays/ex1.java
@@ -0,0 +1,14 @@
+package day2Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ex1 {
+    public static void main(String[] args) {
+        System.out.println("Array and sorting algorithms");
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+
+    }
+}
